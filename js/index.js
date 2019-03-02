4f7444d4db769ffacac926b25a5d6c105cdd9cd1
palpriyanka16 <priyankapal330@gmail.com>
2019-03-02 17:10:20 +0530

    Add basic functionality for view Tx and view BlockChain buttons

---
+++ js/index.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+  $("#viewTxBtn").click(function(){
+  	if($("#inputTransPath").val() == '') {
+  		$("#txDetails").css("display", "none");
+  	}
+  	else {
+  		$("#txDetails").css("display", "block");
+  		// ajax query to fetch transaction details and display response
+		
+	}
+  });
+  $('#viewBCBtn').click(function() {
+
+  	//ajaz query to fetch all blockchain details
+
+  	var $table = $('<table class="table table-striped">');
+	var $tbody = $('<tbody>');
+	var $tr1 = $('<tr>');
+	var $th1 = $('<th>');
+	var $th2 = $('<th>');
+	var $th3 = $('<th>');
+	$th1.text("Block hash");
+	$th2.text("Block height");
+	$th3.text("Block nonce");
+	$tr1.append($th1);
+	$tr1.append($th2);
+	$tr1.append($th3);
+	$tbody.append($tr1);
+
+	
+
+	$table.append($tbody);
+	for (i = 1; i<= 4; i++){
+	    var $tr = $('<tr>');
+	    var $td1 = $('<td>');
+	    var $td2 = $('<td>');
+	    var $td3 = $('<td>');
+	    $td1.text(i);
+	    $td2.text(i);
+	    $td3.text(i);
+	    $tr.append($td1);
+	    $tr.append($td2);
+	    $tr.append($td3);
+	    $tbody.append($tr);
+	};
+	$('#blockChainDiv').html($table);
+  });
+
+
+});
