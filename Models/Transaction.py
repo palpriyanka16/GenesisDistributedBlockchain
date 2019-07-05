7ee265759da3ed2ce395adf1625777182b5ab479
digaru19 <digsblogger@gmail.com>
2019-07-06 00:05:06 +0530

    Remove random factor while generating ID of a transaction

--- Models/Transaction.py
+++ Models/Transaction.py
@@ -18,11 +18,18 @@ class Transaction:
         return "Sender: {} \nData: {}\n".format(self.sender, data_hash)
     
     def get_id(self):
-        t = self.sender + self.data + self.signature + str(randint(1, 3000))
+        t = self.sender + self.data + self.signature
         transaction_hash = sha256(t.encode()).hexdigest()
         return transaction_hash
 
     def verify(self):
+        '''
+            WARNING: Disabling the cryptographic verification of a transaction
+            for developmental purposes only.
+            TODO: Enable this function before final submission.
+        '''
+        return True
+
         # The 'sender' is the PEM format of the sender's RSA public key
         pubkey = rsa.PublicKey.load_pkcs1(self.sender.encode())
         data_hash = sha256(self.data.encode()).hexdigest().encode()
