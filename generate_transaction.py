fa12cfa2909bb211d967a03e0ca800aad473308d
digaru19 <digsblogger@gmail.com>
2019-07-14 23:44:02 +0530

    Modify 'generate_transaction.py' script

--- generate_transaction.py
+++ generate_transaction.py
@@ -4,13 +4,12 @@ import codecs
 import hashlib
 
 privkey_file = input("Enter private key file name (.pem): ")
-
 with open(privkey_file, "rb") as file:
     privkey_file_contents = file.read()
 
 privkey = rsa.PrivateKey.load_pkcs1(privkey_file_contents)
 
-data = input("Enter data to be singed: ")
+data = input("Enter data to be signed: ")
 data_hash = hashlib.sha256(data.encode()).hexdigest().encode()
 
 signature = rsa.sign(data_hash, privkey, 'SHA-256')
@@ -18,9 +17,6 @@ signature_hex = codecs.encode(signature, 'hex').decode()
 
 print()
 print("\nTRANSACTION: \n")
-print("*" * 60)
-print("\nSender: \n")
-print(privkey_file_contents.decode())
 
 print()
 print("*" * 60)
