dc19f3c3a90701ad9b2c2a8b9e864118c8171b82
Rohith Balaji <rohithbalaji123@gmail.com>
2019-10-18 10:57:22 +0530

    Add API to get nonce from data nodes and insert block into blockchain

--- Services/PoolMiningService.py
+++ Services/PoolMiningService.py
@@ -18,9 +18,13 @@ class PoolMiningService:
     writer_service = WriterService.get_instance()
     network_service = NetworkService.get_instance()
     cur_nonce_start_value = 0
-    cur_block_in_buffer = ""
-    cur_block_hash_in_buffer = ""
-
+    cur_block_in_buffer = {
+        "block_data_without_nonce": "",
+        "block_hash": "",
+        "block_number": 0,
+        "prev_block_hash": "",
+        "transactions": {}
+    }
 
     @staticmethod
     def get_instance():
@@ -35,18 +39,39 @@ class PoolMiningService:
             raise Exception("Singleton instance already exists. Use PoolMiningService.get_instance() to get that instance.")
         PoolMiningService.__instance = self
 
+    # function to check if the hash satisfies the difficulty condition in mining
+    def satisfies_difficulty(self, hashed_value_in_hex):
+        # number of bits in the given hash
+        total_number_of_bits = len(hashed_value_in_hex) * 4
+
+        hashed_value_in_int = int(hashed_value_in_hex, 16)
+        # if the hashed value is less that 1 followed by (total_number_of_bits - self.__mining_difficulty) zeros
+        # in the binary form, it means the hashed value has a minimum of self.__mining_difficulty zeros in the
+        # beginning
+        return hashed_value_in_int < 2**(total_number_of_bits - self.__mining_difficulty)
+
     def mine_new_transaction_set(self, transaction_list):
         if len(transaction_list) > PoolMiningService.TRANSACTIONS_PER_BLOCK:
             raise Exception("Transactions count should be less than " + str(PoolMiningService.TRANSACTIONS_PER_BLOCK) + 1)
 
+        transactions = {}
+        for t in transaction_list:
+            transactions[t.get_id()] = t
         block_number = self.writer_service.get_head_block_number() + 1
         prev_block_hash = self.writer_service.get_head_block_hash()
         block_data_without_nonce = Block.block_data_without_nonce(block_number, prev_block_hash, transaction_list)
-        self.cur_block_in_buffer = block_data_without_nonce
-        self.cur_block_hash_in_buffer = md5(block_data_without_nonce.encode()).hexdigest()
 
-        block_with_nonce_range = {}
-        block_with_nonce_range["block"] = block_data_without_nonce
+        self.cur_block_in_buffer = {
+            "block_data_without_nonce": block_data_without_nonce,
+            "block_hash": md5(block_data_without_nonce.encode()).hexdigest(),
+            "block_number": block_number,
+            "prev_block_hash": prev_block_hash,
+            "transactions": transactions
+        }
+
+        block_with_nonce_range = {
+            "block": block_data_without_nonce
+        }
 
         for data_node in self.DATA_NODES_ADDRESS:
             block_with_nonce_range["nonce_start"] = self.cur_nonce_start_value
@@ -57,15 +82,41 @@ class PoolMiningService:
 
     def reset_pool(self):
         self.cur_nonce_start_value = 0
-        self.cur_block_in_buffer = ""
-        self.cur_block_hash_in_buffer = ""
+        self.cur_block_in_buffer = {
+            "block_data_without_nonce": "",
+            "block_hash": "",
+            "block_number": 0,
+            "prev_block_hash": "",
+            "transactions": {}
+        }
 
     def send_next_nonce_range(self, to):
-        block_with_nonce_range = {}
-        block_with_nonce_range["block"] = self.cur_block_in_buffer
-        block_with_nonce_range["block_hash"] = self.cur_block_hash_in_buffer
-        block_with_nonce_range["nonce_start"] = self.cur_nonce_start_value
-        block_with_nonce_range["nonce_end"] = self.cur_nonce_start_value + self.NONCE_RANGE_PER_NODE
+        block_with_nonce_range = {
+            "block": self.cur_block_in_buffer["block_data_without_nonce"],
+            "nonce_start": self.cur_nonce_start_value,
+            "nonce_end": self.cur_nonce_start_value + self.NONCE_RANGE_PER_NODE
+        }
 
         self.network_service.send_block_for_mining(to, block_with_nonce_range)
         self.cur_nonce_start_value += self.NONCE_RANGE_PER_NODE
+
+    def validate_and_add_block(self, nonce, block_hash):
+        if block_hash != self.cur_block_in_buffer["block_hash"]:
+            return
+
+        block_data = self.cur_block_in_buffer["block_data_without_nonce"] + str(nonce)
+        block_data_hash = md5(block_data.encode()).hexdigest()
+        if not self.satisfies_difficulty(block_data_hash):
+            logging.error("Nonce of the block does not meet mining criteria")
+            return
+
+        self.reset_pool()
+        block = Block(
+            self.cur_block_in_buffer["block_number"],
+            self.cur_block_in_buffer["prev_block_hash"],
+            self.cur_block_in_buffer["transactions"],
+            nonce,
+            block_data_hash
+        )
+        logging.info(block.convert_to_dict())
+        self.writer_service.write(block_data_hash, block)
