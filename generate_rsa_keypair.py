7388d49fc2d46ff6c4f699be23bb46dffde76d1d
Shubham Dighe <shubhamdighe@alphonso.tv>
2019-10-30 20:52:09 +0530

    Add KEY_SIZE as command line arguments in gen_rsa-keypair.py

--- generate_rsa_keypair.py
+++ generate_rsa_keypair.py
@@ -1,11 +1,15 @@
-
+import sys
 import rsa
 
-KEY_SIZE = 512
+KEY_SIZE = int(sys.argv[1], 10)
+
+if not (KEY_SIZE >= 512 and (KEY_SIZE & (KEY_SIZE-1)) == 0):
+    print("Invalid/Small key size")
+    exit(1)
 
 base_name = input("Enter a base name for the key pairs: ")
 
-(pubkey, privkey) = rsa.newkeys(512)
+(pubkey, privkey) = rsa.newkeys(KEY_SIZE)
 pubkey_pem = pubkey.save_pkcs1("PEM")
 privkey_pem = privkey.save_pkcs1("PEM")
 
